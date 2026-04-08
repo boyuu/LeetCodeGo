373d89f5f9d24532e4a0fb4b2a2611cc596d1e21
Yu Bo <463724599@qq.com>
2026-04-08 22:58:21 +0800

    rotate image

---
+++ array/easy/rotate_image/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	rotate(matrix)
+	fmt.Println(matrix)
+}
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ {
+		start, end := 0, n-1
+		for start < end {
+			matrix[i][start], matrix[i][end] = matrix[i][end], matrix[i][start]
+			start++
+			end--
+		}
+	}
+}
